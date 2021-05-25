1d586f5b925919cc29ea8c46940d5469eec75355
David Pessanha <davidppessanha@gmail.com>
2021-05-25 15:31:33 -0300

    Added edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,21 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   # List all tasks
   get 'tasks', to: 'tasks#index', as: :tasks
-  
-  # List one restaurant
+
+  # Get the new task form
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # The route which will receive the submission of the form
+  post 'tasks', to: 'tasks#create'
+
+  # Get the existing task form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # The route which will receive the submission of the form
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # List one task
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+
+
+
 end
