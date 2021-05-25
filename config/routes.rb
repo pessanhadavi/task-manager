5f85f1fcef3660584dc01ee68eff14d79670c632
David Pessanha <davidppessanha@gmail.com>
2021-05-25 14:01:07 -0300

    Added list and show features

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'tasks#index'
+  # List all tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+  
+  # List one restaurant
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
